dc403c799f2d04e8c77bf97f5f0329e7999d8418
Stuart Williams <stuart.williams@bbc.co.uk>
2018-03-16 09:02:29 +0000

    Added 'spdx-exceptions', 'spdx-license-ids' and 'ua-parser-js' to exceptions

--- scripts/exceptions.js
+++ scripts/exceptions.js
@@ -7,5 +7,14 @@ module.exports = {
   },
   "map-stream@0.1.0": {
     "reason": "MIT License; see: https://github.com/dominictarr/map-stream"
+  },
+  "spdx-exceptions@2.1.0": {
+    "reason": "Public Domain; see https://github.com/kemitchell/spdx-exceptions.json"
+  },
+  "spdx-license-ids@3.0.0": {
+    "reason": "Public Domain; see https://github.com/shinnn/spdx-license-ids"
+  },
+  "ua-parser-js@0.7.17": {
+    "reason": "Public Domain; see https://github.com/faisalman/ua-parser-js"
   }
 }
