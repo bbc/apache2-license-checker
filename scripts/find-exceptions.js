46dea1956fe1fb684eda51dcc47a0b3f986766bf
John Beech <johnbeech@users.noreply.github.com>
2019-02-26 15:27:50 +0000

    Local exceptions (#9)
    * Replace exceptions.js with a local license-exceptions.json file
    * Remove external exceptions from license-exceptions.json
    * Update README.md
    * Add patch, preversion, and postversion scripts to package.json

---
+++ scripts/find-exceptions.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const path = require('path')
+const colors = require('colors/safe')
+
+function findExceptions() {
+  const exceptionsFilePath = path.join(process.cwd(), 'license-exceptions.json')
+  const file = readFile(exceptionsFilePath)
+  const exceptions = parseExceptionsFile(file)
+
+  return exceptions
+}
+
+function readFile(filePath) {
+  let file
+  try {
+    file = fs.readFileSync(filePath, 'utf8')
+  }
+  catch(ex) {
+    console.info('Checked for local license-exceptions.json; but no file was found.')
+  }
+  return file
+}
+
+function parseExceptionsFile(fileContents) {
+  let exceptions
+  if (fileContents) {
+    try {
+      exceptions = JSON.parse(fileContents).exceptions
+      console.log(`Found ${Object.keys(exceptions).length} local exceptions in liscense-exceptions.json:`)
+      Object.keys(exceptions).forEach((name) => {
+        const reason = exceptions[name].reason || 'No reason given'
+        console.log(colors.blue(`  - ${name} : ${reason}`))
+      })
+      console.log('')
+    }
+    catch(ex) {
+      console.error('Found a local license-exceptions.json; but there was a JSON parse error:')
+      console.error(fileContents)
+    }
+  }
+  return exceptions
+}
+
+module.exports = findExceptions()
